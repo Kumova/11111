65d3505b74f7863d6d941af0a204ae3a2bc1b377
Daria <you@example.com>
2022-11-05 10:44:41 +0300

    ---

---
+++ work_with_database/phones/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Phone(models.Model):
+    # TODO: Добавьте требуемые поля
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.CharField(max_length=30)
+    image = models.ImageField()
+    release_date = models.DateTimeField()
+    lte_exists = models.CharField(max_length=200, default=True)
+    slug = models.SlugField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name}, {self.price}: {self.image}'
+
